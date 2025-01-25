20752db883bfb2561128ed97ea0d663d2d424e21
jdidi422 <rassiljdidi3@gmail.com>
2025-01-25 23:39:40 +0100

    chnage

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary.update({key: value})
+    return a_dictionary 
 
-    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
-    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
-    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
-    b2 = tuple_b[1] if len(tuple_b) > 1 else 0 
-
-    return (a1 + b1, a2 + b2)
