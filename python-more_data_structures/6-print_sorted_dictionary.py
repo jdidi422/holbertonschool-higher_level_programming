078e2aea5078666337664cb40f8babd418d96b91
jdidi422 <rassiljdidi3@gmail.com>
2025-01-25 23:45:14 +0100

    data

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    """
-    Prints a dictionary by ordered keys
-    """
-    for key in sorted(a_dictionary.keys()):  
-        print(f"{key}: {a_dictionary[key]}")
+    for x, y in sorted(a_dictionary.items()):
+        print("{}: {}".format(x, y))
