cfb1bd4ea377da451659ed22b6ec40e140c174b1
jdidi422 <rassiljdidi3@gmail.com>
2025-01-29 10:23:51 +0100

    python-classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Task 1: Class Square
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            size (int): The size of the square.
+        """
+        self.__size = size
