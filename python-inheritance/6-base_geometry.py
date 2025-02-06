6d14baf1733cc40a4f35300e8c5a80775f09206a
jdidi422 <rassiljdidi3@gmail.com>
2025-02-06 13:55:43 +0100

    chnage

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+
+
+class BaseGeometry:
+    """
+    function documentation
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
