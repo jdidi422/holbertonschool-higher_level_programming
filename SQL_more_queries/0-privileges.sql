339bb09bd00997f05fd8b76d9774a5fd7e0b1b42
jdidi422 <rassiljdidi3@gmail.com>
2025-03-12 00:09:54 +0100

    chnage

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,12 +1,4 @@
--- Créer les utilisateurs s'ils n'existent pas
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Accorder des privilèges pour éviter l'erreur (à modifier selon tes besoins)
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Afficher les privilèges des utilisateurs
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
